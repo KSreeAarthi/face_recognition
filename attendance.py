8ab63813bdc2ba832dea15145e96b17b58387119
KSreeAarthi <106153702+KSreeAarthi@users.noreply.github.com>
2022-05-29 09:47:22 +0530

    Add files via upload

---
+++ attendance.py
@@ -0,0 +1,79 @@
+#importing libraries
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+path = 'images'              #folder having all images
+images = []
+Picture_Name = []
+myList = os.listdir(path)
+print(myList)                 #displaying the images list
+
+
+for pre_img in myList:
+    present_img = cv2.imread(f'{path}/{pre_img}')
+    images.append(present_img)
+    Picture_Name.append(os.path.splitext(pre_img)[0])
+print(Picture_Name)           #extarcting name of the face from the file name
+
+
+def faceEncodings(images):     #extracting the encodings of given pictures
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def attendance(name):                     #attendance recording system
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            time_now = datetime.now()
+            tStr = time_now.strftime('%H:%M:%S')
+            dStr = time_now.strftime('%d/%m/%Y')
+            f.writelines(f'\n{name},{tStr},{dStr}')
+
+
+encodeListKnown = faceEncodings(images)
+print('Completed Encoding')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    faces = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+    faces = cv2.cvtColor(faces, cv2.COLOR_BGR2RGB)
+
+    facesCurrentFrame = face_recognition.face_locations(faces)
+    encodesCurrentFrame = face_recognition.face_encodings(faces, facesCurrentFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = Picture_Name[matchIndex].upper()
+            # print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            attendance(name)
+
+    cv2.imshow('Webcam', frame)
+    if cv2.waitKey(1) == 13:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
